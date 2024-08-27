91185f335cefaa782d8747285a391c75b6f0b2bf
BaxterMcKenzie <baxmckenzie@gmail.com>
2024-08-27 20:38:25 +1200

    projectModel set up

---
+++ backend/models/projectModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const projectSchema = new Schema ({
+    project_name: {
+        type: String,
+        require: true
+    },
+    project_img: {
+        type: String,
+        require: true
+    },
+    author_name: {
+        type: String,
+        require: true
+    },
+    author_img: {
+        type: String,
+        require: true
+    },
+    description: {
+        type: String,
+        require: true
+    },
+    github_repo: {
+        type: String,
+    },
+   vercel_link: {
+        type: String,
+    },
+    github_profile: {
+        type: String,
+    },
+
+}, {timestamps: true});
+
+module.exports = mongoose.model('Project', projectSchema)
