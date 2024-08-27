735f1d4f8cc6285c20b1b53f43c6d424a6fb2fbd
Yasmin Feisst <yasminfeisst99@gmail.com>
2024-08-27 17:00:33 +1200

    Functionallity for header component complete
    Added basic Signup, Login, and Form components for this
    
    Will need to go in and complete additional CSS when site is more put together

---
+++ frontend/src/components/nav/form/Form.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './form.scss'
+
+const Form = () => {
+  return (
+    <div className='form'>
+      FORM
+    </div>
+  )
+}
+
+export default Form
--- frontend/src/components/nav/header/Header.jsx
+++ frontend/src/components/nav/header/Header.jsx
@@ -1,25 +1,32 @@
+import { useState } from 'react'
+import Form from '../form/Form'
 import { Link } from 'react-router-dom'
+import './header.scss'
 
 const Header = () => {
 
+    const [formIsOpen, openForm] = useState(false)
+
+    const toggleForm = () => {
+        openForm(!formIsOpen);
+        document.body.classList.toggle('no-scroll')
+    }
+
     return (
         <>
-            <div id='topNav'>
-                {/* LOGO */}
-                <div id="logo">
-                    <Link to="/">WUX</Link>
+            <div className='header'>
+                <div className='user-actions'>
+                    <Link className='nav-link' to='/login'> Login </Link>
+                    <Link className='nav-link' to='/signup'> Signup </Link>
                 </div>
-
-                {/* DESKTOP MENU */}
-                <ul id='menu'>
-                    <li>
-                        <Link to='/'>Home</Link>
-                    </li>
-                    <li>
-                        <Link to='/projects'>Projects</Link>
-                    </li>
-                </ul>
+                <div className='nav-links'>
+                    <Link className='nav-link' to='/'> Home </Link>
+                    <Link className='nav-link' to='/projects'> Projects </Link>
+                </div>
+                <div className='form-button' onClick={toggleForm}> Add Project </div>
             </div>
+
+            {formIsOpen && <Form closeMethod={toggleForm}/>}
         </>
     )
 }
