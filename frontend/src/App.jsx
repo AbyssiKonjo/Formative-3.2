2f896c2417dafffbd25165f7884355c870dbeed7
BaxterMcKenzie <baxmckenzie@gmail.com>
2024-08-30 11:13:14 +1200

    CSS single page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
         <Route path='/projects' element={<Projects/>}/>
         <Route path='/login' element={!user ? <Login/> : <Navigate to="/"/>}/>
         <Route path='/signup' element={!user ? <Signup/> : <Navigate to="/"/>}/>
-        <Route path='/singlestudentproject' element={<SingleStudentProject/>}/>
+        <Route path='/:id' element={<SingleStudentProject/>}/>
       </Routes>
       <Footer/>
     </BrowserRouter>
