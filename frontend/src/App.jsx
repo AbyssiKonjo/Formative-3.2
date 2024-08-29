75fda68eaff29bcf9188440b3bec23534af2c208
Abyssinia Getachew <abyssinia.m.getachew@gmail.com>
2024-08-30 10:54:44 +1200

    nav & projects showing when no user is signed in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,6 +20,7 @@ const App = () => {
       <Routes>
         <Route exact path='/' element={<Home/>}/>
         <Route path='/projects' element={<Projects/>}/>
+        {/* <Route path='/projects' element={user ? <Projects/> : <Navigate to="/projects"/>}/> */}
         <Route path='/login' element={!user ? <Login/> : <Navigate to="/"/>}/>
         <Route path='/signup' element={!user ? <Signup/> : <Navigate to="/"/>}/>
         <Route path='/singlestudentproject' element={<SingleStudentProject/>}/>
