ee758bc84bdc758ab9b537908383fdaedd76264a
BaxterMcKenzie <baxmckenzie@gmail.com>
2024-08-29 18:55:59 +1200

    got the single page to display & did a bit of CSS

--- frontend/src/pages/singleproject/SingleStudentProject.jsx
+++ frontend/src/pages/singleproject/SingleStudentProject.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import './singlestudentproject.scss'
+import { useProjectContext } from '../../hooks/useProjectContext'
 
 const SingleStudentProject = () => {
   const navigate = useNavigate()
+  const { id } = useParams()
+  const {projects} = useProjectContext()
+
+  const project = projects.find(proj => proj._id === id)
+
   return (
     <div className='single-page-project'>
 
@@ -11,9 +17,16 @@ const SingleStudentProject = () => {
         <button className='back-button' onClick={()=>navigate(-1)}>Go Back</button>
       </div>
 
-      <h2>Student Project</h2>
-      {/* <p>{project.author_name}</p>
-      <h3>{project.project_name}</h3> */}
+      <div className='title'>
+        <h2>{project.author_name}'s {project.project_name} Project</h2>
+      </div>
+
+      <div className='single-page-card'>
+        <div className='overlay'>
+          <p>{project.author_name}</p>
+          <h3>{project.project_name}</h3>
+        </div>
+      </div>
 
     </div>
   )
