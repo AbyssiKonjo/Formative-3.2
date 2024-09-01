54921e7c43f71bc272f54b795e37e715ab272f94
Abyssinia Getachew <abyssinia.m.getachew@gmail.com>
2024-09-02 10:23:09 +1200

    profile icon done allong with styling

--- frontend/src/components/nav/header/Header.jsx
+++ frontend/src/components/nav/header/Header.jsx
@@ -28,8 +28,11 @@ const Header = () => {
         <>
             <div className='header'>
                 {user && <div className='logout-div'>
-                    <img src={`http://localhost:4000/public/uploads/${user.profile_image}`}/>
-                
+                    {/* <img src={`http://localhost:4000/public/uploads/${user.profile_image}`}/> */}
+                    <div className='profile-avatar'>
+                       <span>{user.username.charAt(0).toUpperCase()}</span> 
+                    </div>
+                    
                     <span className='nav-username'>{user.username}</span>
                     <div className='logout-button' onClick={handleClick}>Logout</div>
                 </div>}
