975f3875d31f519d15acd662cbdc4d6a8ed5f9e1
BaxterMcKenzie <baxmckenzie@gmail.com>
2024-09-02 10:42:16 +1200

    first commit

--- frontend/src/pages/singleproject/SingleStudentProject.jsx
+++ frontend/src/pages/singleproject/SingleStudentProject.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import './singlestudentproject.scss'
 import { useProjectContext } from '../../hooks/useProjectContext'
+import { useAuthContext } from '../../hooks/useAuthContext'
 import { FaGithub } from "react-icons/fa";
+import { IoLogoVercel } from "react-icons/io5";
 
 const SingleStudentProject = () => {
   const navigate = useNavigate()
   const { id } = useParams()
   const {projects} = useProjectContext()
+
+  const user = JSON.parse(localStorage.getItem('user'))
+  const githubProfile = user.github_profile;
+  console.log(user); 
 
   const project = projects.find(proj => proj._id === id)
 
@@ -34,13 +40,22 @@ const SingleStudentProject = () => {
           <h3>{project.project_name}</h3><br/>
           <p>{project.description}</p><br/>
           <h4>{project.author_name}</h4><br/>
+          <p>Github Profile: <a href={githubProfile} target="_blank" rel="noopener noreferrer">{githubProfile}</a></p><br/>
 
           <div className='socials'>
+
             <a href={project.github_repo} target="_blank" rel="noopener noreferrer">
               <p className='social-icons'>
                 <FaGithub className='github'/>
               </p>
             </a>
+
+            <a href={project.vercel_link} target="_blank" rel="noopener noreferrer">
+              <p className='social-icons'>
+              <IoLogoVercel className='vercel' />
+              </p>
+            </a>
+
           </div>
 
         </div>
